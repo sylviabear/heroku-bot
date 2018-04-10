bc14a3ff6fae348cb88f688b369ecaf7156b8eda
sylviabear <38234383+sylviabear@users.noreply.github.com>
2018-04-09 19:44:12 -0700

    Update ChatterBot.py

--- ChatterBot.py
+++ ChatterBot.py
@@ -4,10 +4,10 @@ import json
 import time
 
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+consumer_key = "YScGj4M4Oe3iyJIK0X7Ou14cx"
+consumer_secret = "IQeC6itNFMmedpDuyp1hrO4f1JXHBRNWuR0U8UtjYbjOF80OE6"
+access_token = "983530416182255616-3cVUmtGd04By1mAuNhU2FjprvIC8HVA"
+access_token_secret = "B7TBjmUw8YEIRbdl5yCwaTMabrDywIub0ckNqrZn9tZDP"
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -17,3 +17,4 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 # Create a function that tweets
 # CODE GOES HERE
+api.update_status("Hello, world!")
